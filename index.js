cb393638406290b255cf291921f53720eb15fb7c
Deanna Baykina <52352148+dbaykina@users.noreply.github.com>
2021-01-14 21:48:47 +0300

    display task array

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const task = { 
-    id: "1", 
-    text: "выучить html",
-    completed: true }
+'use strict';
 
+
+const tasksList = [
+    { id: "1", text: "выучить html", completed: true },
+    { id: "2", text: "выучить css", completed: true },
+    { id: "3", text: "выучить js", completed: false },
+    { id: "4", text: "выучить фреймворк", completed: false },
+    { id: "5", text: "написать несколько учебных проектов", completed: false },
+    { id: "6", text: "пройти собеседование", completed: false },
+    { id: "7", text: "получить работу", completed: false }
+    ];
 
 const createListItem = (task) => {
 let completeClass= '';
@@ -10,20 +17,26 @@ if (task.completed == true) {
     completeClass = 'completed';
 }
   return  `<li data-id="${task.id}" class ="${completeClass}">
-            <div class="view">
-                <input class="toggle" type="checkbox">
-                <label>${task.text}</label>
-            <button></button>
-            </div>
-        </li>`;
+    <div class="view">
+        <input class="toggle" type="checkbox">
+        <button class="destroy"></button>
+        <label>${task.text}</label>
+       <button></button>
+   </div>
+</li>    
+`;
 
 }
 
 
 const renderTask = () => {
         const toDoList = document.querySelector('.todo-list');
-        const el = createListItem(task);
-        toDoList.insertAdjacentHTML('afterbegin', el);
+        toDoList.innerHTML = '';
+        tasksList.forEach((item) => {
+            const itemHTML = createListItem(item);
+            toDoList.insertAdjacentHTML('afterbegin', itemHTML);
+        })
+ 
 }
 
 renderTask();
