80c6c3fcb9ea8b5356a50af6120408b6af7cce8a
Deanna Baykina <52352148+dbaykina@users.noreply.github.com>
2021-01-16 19:42:06 +0300

    function renderTasks has been renamed

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-'use strict';
-
+"use strict";
 
 const tasksList = [
-    { id: "1", text: "выучить html", completed: true },
-    { id: "2", text: "выучить css", completed: true },
-    { id: "3", text: "выучить js", completed: false },
-    { id: "4", text: "выучить фреймворк", completed: false },
-    { id: "5", text: "написать несколько учебных проектов", completed: false },
-    { id: "6", text: "пройти собеседование", completed: false },
-    { id: "7", text: "получить работу", completed: false }
-    ];
+  { id: "1", text: "выучить html", completed: true },
+  { id: "2", text: "выучить css", completed: true },
+  { id: "3", text: "выучить js", completed: false },
+  { id: "4", text: "выучить фреймворк", completed: false },
+  { id: "5", text: "написать несколько учебных проектов", completed: false },
+  { id: "6", text: "пройти собеседование", completed: false },
+  { id: "7", text: "получить работу", completed: false },
+];
 
 const createListItem = (task) => {
-let completeClass= '';
-if (task.completed == true) {
-    completeClass = 'completed';
-}
-  return  `<li data-id="${task.id}" class ="${completeClass}">
+  let completeClass = "";
+  if (task.completed == true) {
+    completeClass = "completed";
+  }
+  return `<li data-id="${task.id}" class ="${completeClass}">
     <div class="view">
         <input class="toggle" type="checkbox">
         <button class="destroy"></button>
@@ -25,18 +24,15 @@ if (task.completed == true) {
    </div>
 </li>    
 `;
+};
 
-}
+const renderTasks = () => {
+  const toDoList = document.querySelector(".todo-list");
+  toDoList.innerHTML = "";
+  tasksList.forEach((item) => {
+    const itemHTML = createListItem(item);
+    toDoList.insertAdjacentHTML("afterbegin", itemHTML);
+  });
+};
 
-
-const renderTask = () => {
-        const toDoList = document.querySelector('.todo-list');
-        toDoList.innerHTML = '';
-        tasksList.forEach((item) => {
-            const itemHTML = createListItem(item);
-            toDoList.insertAdjacentHTML('afterbegin', itemHTML);
-        })
- 
-}
-
-renderTask();
+renderTasks();
